bc6773094e1b159f3685062bb502e11f54730791
Edson Junior <edsonsjr14@outlook.com>
2019-01-08 14:21:34 -0200

    Criando App sem Api

--- src/main.js
+++ src/main.js
@@ -1,20 +1,56 @@
+class App {
+    constructor(){
+        this.repositories = [];
 
-import axios from 'axios';
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+        this.registerHandlers();
+    }
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+    
+    addRepository(event) {
+        event.preventDefault();
 
-class Api {
-    static async getUserInfo(username){
-        
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        }catch (err) {
-            console.warn('Erro na API');
-        }
-        
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida a sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+        });
 
-        
+       this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
     }
 }
 
-Api.getUserInfo('edsonsjr14');
-Api.getUserInfo('edsonsjr144');
+
+new App();
+

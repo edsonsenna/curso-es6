5b563937e43d8618f77cfc6472d86c82cabcef7a
Edson Junior <edsonsjr14@outlook.com>
2019-01-08 12:16:47 -0200

    Axios Configurado/ Req Api Github

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('OK')}, 2000);
-});
 
-async function executaPromise() {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+import axios from 'axios';
 
+class Api {
+    static async getUserInfo(username){
+        
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        }catch (err) {
+            console.warn('Erro na API');
+        }
+        
+
+        
+    }
 }
 
-executaPromise();
+Api.getUserInfo('edsonsjr14');
+Api.getUserInfo('edsonsjr144');
